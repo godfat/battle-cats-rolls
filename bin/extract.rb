09db2c6e4ce0b758646d3462e778d981c17634e6
Lin Jen-Shin <godfat@godfat.org>
2018-06-21 01:59:42 +0800

    Introduce Extractor

--- bin/extract.rb
+++ bin/extract.rb
@@ -1,25 +1,26 @@
 
+require_relative '../lib/battle-cats-rolls/extractor'
 require_relative '../lib/battle-cats-rolls/unpacker'
 
 require 'fileutils'
 
-list_file = ARGV.first || 'data/7.1.0/app/DataLocal.list'
-pack_file = list_file[0...list_file.rindex('.')] + '.pack'
-pack_data = File.binread(pack_file)
-dir = "extract/7.1.0/#{File.basename(pack_file)}"
+extractor = BattleCatsRolls::Extractor.new(
+  ARGV.first || 'data/7.1.0/app/DataLocal.list')
+
 unpacker = BattleCatsRolls::Unpacker.for_pack
 
+dir = "extract/7.1.0/#{File.basename(extractor.pack_path)}"
 FileUtils.mkdir_p(dir)
 
-puts "Extracting #{pack_file}"
+puts "Extracting #{extractor.pack_path}"
 
 # Drop first line for number of files
 list = BattleCatsRolls::Unpacker.for_list.
-  decrypt(File.binread(list_file)).lines.drop(1)
+  decrypt(extractor.list_data).lines.drop(1)
 
 list.each do |row|
   name, offset, size = row.split(',')
 
-  data = unpacker.decrypt(pack_data[offset.to_i, size.to_i])
+  data = unpacker.decrypt(extractor.pack_data[offset.to_i, size.to_i])
   File.binwrite("#{dir}/#{name}", data)
 end
---
+++ lib/battle-cats-rolls/extractor.rb
@@ -0,0 +1,18 @@
+
+module BattleCatsRolls
+  class Extractor < Struct.new(:list_path, :pack_path)
+    def initialize(new_list_path)
+      name = new_list_path[0...new_list_path.rindex('.')]
+
+      super(new_list_path, "#{name}.pack")
+    end
+
+    def list_data
+      @list_data ||= File.binread(list_path)
+    end
+
+    def pack_data
+      @pack_data ||= File.binread(pack_path)
+    end
+  end
+end
