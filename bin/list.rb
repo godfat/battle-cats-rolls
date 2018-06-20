296c8f7b91e0723bee3cd8baca45285e73946e65
Lin Jen-Shin <godfat@godfat.org>
2018-06-21 01:40:02 +0800

    Remove unused method

--- bin/list.rb
+++ bin/list.rb
@@ -3,13 +3,6 @@ require_relative '../lib/battle-cats-rolls/unpacker'
 
 require 'digest/md5'
 
-def decrypt key, data
-  cipher = OpenSSL::Cipher.new('aes-128-ecb')
-  cipher.decrypt
-  cipher.key = key
-  cipher.update(data) + cipher.final
-end
-
 key = Digest::MD5.hexdigest('pack')[0, 16]
 unpacker = BattleCatsRolls::Unpacker.new
 
