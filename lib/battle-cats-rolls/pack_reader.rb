2eb42c6f61127e83a1141184d3c7c4fa6a85f92e
Lin Jen-Shin <godfat@godfat.org>
2018-06-26 18:34:28 +0800

    Absorb Extractor

--- lib/battle-cats-rolls/pack_reader.rb
+++ lib/battle-cats-rolls/pack_reader.rb
@@ -1,23 +1,23 @@
 
-require_relative 'extractor'
 require_relative 'unpacker'
 
 require 'forwardable'
 
 module BattleCatsRolls
   class PackReader < Struct.new(
-    :extractor, :list_unpacker, :pack_unpacker, :name)
+    :list_path, :pack_path, :list_unpacker, :pack_unpacker, :name)
 
     extend Forwardable
 
-    def_delegators :extractor, :list_path, :pack_path
-
     def initialize new_list_path
+      pathname = new_list_path[0...new_list_path.rindex('.')]
+
       super(
-        Extractor.new(new_list_path),
+        new_list_path,
+        "#{pathname}.pack",
         Unpacker.for_list,
         Unpacker.for_pack,
-        File.basename(new_list_path, '.list'))
+        File.basename(pathname))
     end
 
     def each
@@ -32,15 +32,24 @@ module BattleCatsRolls
 
     def list_lines
       # Drop first line for number of files
-      @list_lines ||= list_unpacker.decrypt(extractor.list_data).lines.drop(1)
+      @list_lines ||= list_unpacker.decrypt(list_data).lines.drop(1)
     end
 
     def read line
       filename, offset, size = line.split(',')
-      data = pack_unpacker.decrypt(
-        extractor.pack_data[offset.to_i, size.to_i])
+      data = pack_unpacker.decrypt(pack_data[offset.to_i, size.to_i])
 
       [filename, data]
+    end
+
+    private
+
+    def list_data
+      @list_data ||= File.binread(list_path)
+    end
+
+    def pack_data
+      @pack_data ||= File.binread(pack_path)
     end
   end
 end
