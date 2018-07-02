e15efacdb507bc76ca8e0cf1a651216d69f6c6d0
Lin Jen-Shin <godfat@godfat.org>
2018-07-03 01:09:24 +0800

    Prepare some methods for not advancing seed state

--- bin/roll.rb
+++ bin/roll.rb
@@ -4,13 +4,13 @@ require_relative '../lib/battle-cats-rolls/gacha'
 
 ball = BattleCatsRolls::CrystalBall.load('build/7.1.0')
 
-[302].each do |event_id|
+[273, 302].each do |event_id|
   gacha = BattleCatsRolls::Gacha.new(ball, event_id, ARGV.first.to_i)
 
   puts "Rolling (#{gacha.ubers.size} ubers) #{gacha.name}"
 
   10.times do
-    puts gacha.roll
+    puts gacha.roll!
   end
 
   puts
--- lib/battle-cats-rolls/gacha.rb
+++ lib/battle-cats-rolls/gacha.rb
@@ -14,11 +14,11 @@ module BattleCatsRolls
       super(GachaPool.new(crystal_ball, event_id), seed)
     end
 
-    def roll
-      rarity = roll_rarity
-      slot = roll_slot(rarity)
+    def roll!
+      rarity = roll_rarity!
+      slot = roll_slot!(rarity)
 
-      pool.dig_cat(rarity, pool.dig_slot(rarity, slot))
+      dig_cat(rarity, slot)
     end
 
     def ubers
@@ -27,10 +27,14 @@ module BattleCatsRolls
 
     private
 
-    def roll_rarity
+    def dig_cat rarity, slot
+      pool.dig_cat(rarity, pool.dig_slot(rarity, slot))
+    end
+
+    def roll_rarity!
       base = 10000
 
-      case roll_score % base
+      case roll_score! % base
       when 0...(base - sr - ssr)
         2
       when sr...(base - ssr)
@@ -40,25 +44,29 @@ module BattleCatsRolls
       end
     end
 
-    def roll_slot rarity
-      roll_score % pool.dig_slot(rarity).size
+    def roll_slot! rarity
+      roll_score! % pool.dig_slot(rarity).size
     end
 
-    def roll_score
+    def roll_score!
       score = seed.abs
-      advance_seed
+      advance_seed!
       score
     end
 
-    def advance_seed
-      self.seed = shift(:<<, 13)
-      self.seed = shift(:>>, 17)
-      self.seed = shift(:<<, 15)
+    def advance_seed!
+      self.seed = advance_seed
     end
 
-    def shift direction, bits
-      shifted = seed ^ seed.public_send(direction, bits)
-      (shifted + 0x80000000) % (0x100000000) - (0x80000000)
+    def advance_seed base_seed=seed
+      base_seed = shift(:<<, 13, base_seed)
+      base_seed = shift(:>>, 17, base_seed)
+      base_seed = shift(:<<, 15, base_seed)
+    end
+
+    def shift direction, bits, base_seed=seed
+      base_seed ^= base_seed.public_send(direction, bits)
+      (base_seed + 0x80000000) % (0x100000000) - (0x80000000)
     end
   end
 end
