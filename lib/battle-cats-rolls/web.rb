e1f785a41ad592965d7b4689a002e4f3ecc3d0e4
Lin Jen-Shin <godfat@godfat.org>
2018-07-05 23:29:48 +0800

    Link to the next roll

--- lib/battle-cats-rolls/web.rb
+++ lib/battle-cats-rolls/web.rb
@@ -4,7 +4,10 @@ require_relative 'crystal_ball'
 require_relative 'gacha'
 
 require 'jellyfish'
+
+require 'cgi'
 require 'erb'
+require 'forwardable'
 
 module BattleCatsRolls
   class Web
@@ -12,14 +15,20 @@ module BattleCatsRolls
       @ball ||= CrystalBall.load('build/7.1.0')
     end
 
-    class View < Struct.new(:arg)
+    class View < Struct.new(:controller, :arg)
+      extend Forwardable
+
+      def_delegator :controller, :request
+
       def render name
         erb(:layout){ erb(name) }
       end
 
       def each_ab_cat
         arg[:cats].each.with_index.inject(nil) do |prev_b, (ab, index)|
-          yield(prev_b, ab, index + 1)
+          sequence = index + 1
+
+          yield(prev_b, ab, arg.dig(:cats, sequence, 0), sequence)
 
           ab.last
         end
@@ -30,14 +39,50 @@ module BattleCatsRolls
           step = sequence + arg[:guaranteed_rolls] + offset
 
           if offset < 0
-            "#{name}<br>-&gt; #{step}"
+            "#{h name}<br>-&gt; #{step}"
           else
-            "#{name}<br>&lt;- #{step}"
+            "#{h name}<br>&lt;- #{step}"
           end
         end
       end
 
+      def link_to_roll cat, next_cat
+        if next_cat
+          %Q{<a href="#{uri_to_roll(next_cat)}">#{h cat.name}</a>}
+        else
+          cat.name
+        end
+      end
+
+      def h str
+        CGI.escape_html(str)
+      end
+
+      def u str
+        CGI.escape(str)
+      end
+
       private
+
+      def uri_to_roll cat
+        uri(seed: cat.rarity_seed,
+            event: controller.event,
+            count: controller.count)
+      end
+
+      def uri query={}
+        query_string = query.map do |key, value|
+          "#{u key.to_s}=#{u value.to_s}"
+        end.join('&')
+
+        path = "#{request.base_url}#{request.path}"
+
+        if query.empty?
+          path
+        else
+          "#{path}?#{query_string}"
+        end
+      end
 
       def erb name, &block
         ERB.new(views(name)).result(binding, &block)
@@ -53,12 +98,12 @@ module BattleCatsRolls
         @gacha ||= Gacha.new(Web.ball, event, seed)
       end
 
-      def event
-        @event ||= request.GET['event']
-      end
-
       def seed
         @seed ||= request.GET['seed'].to_i
+      end
+
+      def event
+        @event ||= request.GET['event']
       end
 
       def count
@@ -66,7 +111,7 @@ module BattleCatsRolls
       end
 
       def render name, arg
-        View.new(arg).render(name)
+        View.new(self, arg).render(name)
       end
     end
 
