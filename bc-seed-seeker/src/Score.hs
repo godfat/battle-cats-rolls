f7edd46f6eb4d1d2171c95d911420bd8517e2875
Lin Jen-Shin <godfat@godfat.org>
2018-07-08 21:00:23 +0800

    Split Seed from Score

--- bc-seed-seeker/src/Score.hs
+++ bc-seed-seeker/src/Score.hs
@@ -1,38 +1,12 @@
 
-module Score(Score) where
+module Score(Score(Rarity, Slot), scoreBase) where
 
 import Data.Int (Int32)
 
-base :: Int32
-base = 10000
+scoreBase :: Int32
+scoreBase = 10000
 
 data Score =
   Rarity { begin :: Int32, end :: Int32 }
   | Slot { n :: Int32, count :: Int32 }
-  | Seed { seed :: Int32 }
   deriving (Show)
-
-instance Eq Score where
-  Seed seed == Rarity begin end =
-    score >= begin && score < end where
-    score = seed `mod` base
-
-  Seed seed == Slot n count =
-    slot == n where
-    slot = seed `mod` count
-
-  Seed a == Seed b = a == b
-  Rarity a0 a1 == Rarity b0 b1 = a0 == b0 && a1 == b1
-  Slot a0 a1 == Slot b0 b1 = a0 == b0 && a1 == b1
-
-  rarity@(Rarity _ _) == seed@(Seed _) = seed == rarity
-  slot@(Slot _ _) == seed@(Seed _) = seed == slot
-
-cycle :: [Score]
-cycle = undefined
-
-allCycles :: [[Score]]
-allCycles = undefined
-
-rolls :: [Score]
-rolls = undefined
--- bc-seed-seeker/src/Seed.hs
+++ bc-seed-seeker/src/Seed.hs
@@ -1,11 +1,49 @@
 
-module Seed(advance) where
+module Seed(Seed(Seed), toInt32, advance, matchRoll) where
 
 import Data.Bits (xor, shiftL, shiftR)
 import Data.Int (Int32)
 
+import Score
+
+newtype Seed = Seed { toInt32 :: Int32 } deriving (Show, Eq)
+
+advance :: Seed -> Seed
+advance =
+  Seed .
+  (step (`shiftL` 15)) .
+  (step (`shiftR` 17)) .
+  (step (`shiftL` 13)) .
+  toInt32
+
+matchRoll :: Seed -> Score -> Bool
+matchRoll (Seed seed) (Rarity begin end) =
+  score >= begin && score < end where
+  score = seed `mod` scoreBase
+
+matchRoll (Seed seed) (Slot n count) =
+  slot == n where
+  slot = seed `mod` count
+
 step :: (Int32 -> Int32) -> Int32 -> Int32
 step direction seed = seed `xor` (direction seed)
 
-advance :: Int32 -> Int32
-advance = (step (`shiftL` 15)) . (step (`shiftR` 17)) . (step (`shiftL` 13))
+cycle :: [Seed]
+cycle = undefined
+
+allCycles :: [[Seed]]
+allCycles = undefined
+
+rolls :: [Score]
+rolls = undefined
+
+seed = Seed 1745107336
+tests = [
+  advance seed == Seed 2009320978,
+  matchRoll seed (Rarity 7000 9000),
+  not $ matchRoll seed (Rarity 0 7000),
+  matchRoll seed (Slot 6 10),
+  not $ matchRoll seed (Slot 7 10)
+  ]
+
+test = foldr (&&) True tests
