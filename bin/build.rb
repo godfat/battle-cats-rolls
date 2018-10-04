a1584b2ce5b1e45df72ffbbeddd9f082f6c48d38
Lin Jen-Shin <godfat@godfat.org>
2018-10-04 23:14:39 +0800

    Move bin/build.rb to runner, too

--- bin/build.rb
+++ bin/build.rb
@@ -1,31 +1,4 @@
 
-require_relative '../lib/battle-cats-rolls/pack_provider'
-require_relative '../lib/battle-cats-rolls/cats_builder'
+require_relative '../lib/battle-cats-rolls/runner'
 
-pack = BattleCatsRolls::PackProvider.new('data/7.3.0/app')
-cats_pack = BattleCatsRolls::CatsBuilder.new(pack)
-
-require_relative '../lib/battle-cats-rolls/extract_provider'
-
-extract = BattleCatsRolls::ExtractProvider.new('extract/7.3.0')
-cats_extract = BattleCatsRolls::CatsBuilder.new(extract)
-
-p cats_pack == cats_extract
-
-require_relative '../lib/battle-cats-rolls/tsv_reader'
-
-current = BattleCatsRolls::TsvReader.current
-last_date = current.gacha.
-  delete_if { |_, data| data['platinum'] }.
-  sort_by { |key, data| data['end_on'] }.
-  dig(-1, -1, 'end_on').
-  strftime('%Y%m%d')
-File.write("data/events/#{last_date}.tsv", current.tsv)
-
-require_relative '../lib/battle-cats-rolls/events_reader'
-require_relative '../lib/battle-cats-rolls/crystal_ball'
-
-events = BattleCatsRolls::EventsReader.read('data/events')
-ball = BattleCatsRolls::CrystalBall.from_pack_and_events(cats_pack, events)
-
-ball.dump('build')
+BattleCatsRolls::Runner.build
--- lib/battle-cats-rolls/runner.rb
+++ lib/battle-cats-rolls/runner.rb
@@ -8,6 +8,11 @@ module BattleCatsRolls
       '7.3.0'
     end
 
+    def build
+      write_events
+      write_data
+    end
+
     def extract dir=nil
       require_relative 'pack_reader'
       require 'fileutils'
@@ -35,6 +40,54 @@ module BattleCatsRolls
         puts "#{file}:"
         puts unpacker.decrypt(File.binread(file))
         puts "---"
+      end
+    end
+
+    def write_events
+      puts "Downloading event data..."
+
+      require_relative 'tsv_reader'
+
+      current = TsvReader.current
+      last_date = current.gacha.
+        delete_if { |_, data| data['platinum'] }.
+        sort_by { |key, data| data['end_on'] }.
+        dig(-1, -1, 'end_on').
+        strftime('%Y%m%d')
+      File.write("data/events/#{last_date}.tsv", current.tsv)
+    end
+
+    def write_data
+      require_relative 'events_reader'
+      require_relative 'crystal_ball'
+
+      events = EventsReader.read('data/events')
+      ball = CrystalBall.from_pack_and_events(cats_pack, events)
+
+      puts "Writing data..."
+
+      ball.dump('build')
+    end
+
+    def cats_pack
+      require_relative 'cats_builder'
+
+      CatsBuilder.new(provider)
+    end
+
+    def provider
+      if File.exist?("extract/#{version}")
+        puts "Loading from extract..."
+
+        require_relative 'extract_provider'
+
+        ExtractProvider.new("extract/#{version}")
+      else
+        puts "Loading from pack..."
+
+        require_relative 'pack_provider'
+
+        PackProvider.new("data/#{version}/app")
       end
     end
 
