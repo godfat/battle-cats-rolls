869f60fceea94764108dda1710556839bebb8cdd
Lin Jen-Shin <godfat@godfat.org>
2018-12-08 20:58:50 +0800

    Use Applicative.empty

--- Seeker/Main.hs
+++ Seeker/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import GHC.Conc (numCapabilities)
 import Data.Word (Word32)
+import Control.Applicative (empty)
 import Control.Monad (join)
 import Control.Arrow ((***))
 import System.Posix.Signals (installHandler, sigINT, Handler(CatchOnce))
@@ -15,7 +16,7 @@ import Worker
 main = do
   -- Ignore sigINT once so we can gracefully shutting it down
   -- when the parent process receives sigINT
-  installHandler sigINT (CatchOnce (return ())) Nothing
+  installHandler sigINT (CatchOnce empty) Nothing
 
   result <- map read <$> words <$> getContents :: IO [Word32]
   let picks = sourcePicks (buildSource result)
