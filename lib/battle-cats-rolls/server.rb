1e616c06eae76d6ab1134dec7cc0ee47c6bfc6da
Lin Jen-Shin <godfat@godfat.org>
2018-12-20 14:51:55 +0800

    Omit newline from uptime

--- lib/battle-cats-rolls/server.rb
+++ lib/battle-cats-rolls/server.rb
@@ -51,7 +51,7 @@ module BattleCatsRolls
       until @shutdown do
         printf \
           "Memory total: %.2fM, current: %.2fM, CPU: %.2f%%,%s\n",
-          *ps, `uptime`[/(?<=users,).+\z/m]
+          *ps, `uptime`[/(?<=users,).+/]
         sleep(10)
       end
 
