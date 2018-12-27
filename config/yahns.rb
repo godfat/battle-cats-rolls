1c1e53fc8910f0cef3c47a0c4f60251cb4b95892
Lin Jen-Shin <godfat@godfat.org>
2018-12-28 01:46:05 +0800

    Swap server order

--- config/yahns.rb
+++ config/yahns.rb
@@ -3,17 +3,17 @@ rack, = Rack::Builder.parse_file(
   File.expand_path("#{__dir__}/../config.ru"))
 
 app :rack, rack, preload: true do
-  listen ENV['SEEK_YAHNS'] || 9090
-
-  queue do
-    worker_threads 25
-  end
-end
-
-app :rack, rack, preload: true do
   listen ENV['WEB_YAHNS'] || 8080
 
   queue do
     worker_threads 5
+  end
+end
+
+app :rack, rack, preload: true do
+  listen ENV['SEEK_YAHNS'] || 9090
+
+  queue do
+    worker_threads 25
   end
 end
